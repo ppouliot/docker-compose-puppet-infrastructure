a72328029df5262209747a37d39b31e59cdcc799
Peter Pouliot <peter@pouliot.net>
2018-08-07 00:08:22 -0400

    updating

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,10 +4,10 @@ read sshkeys
 
 
 docker-compose up -d ;
-docker exec -it puppet sh -c 'puppetserver gem install msgpack'
-docker exec -it puppet sh -c 'gem install generate-puppetfile ra10ke hiera-eyaml slack-notifier'
-docker exec -it puppet sh -c 'mkdir -p /etc/puppetlabs/r10k'
-docker exec -it puppet sh -c 'mv /etc/puppetlabs/code/environments/production /etc/puppetlabs/code/environments/production.orig'
+docker exec -t puppet sh -c 'puppetserver gem install msgpack'
+docker exec -t puppet sh -c 'gem install generate-puppetfile ra10ke hiera-eyaml slack-notifier'
+docker exec -t puppet sh -c 'mkdir -p /etc/puppetlabs/r10k'
+docker exec -t puppet sh -c 'mv /etc/puppetlabs/code/environments/production /etc/puppetlabs/code/environments/production.orig'
 docker cp ./hiera.yaml puppet:/etc/puppetlabs/hiera.yaml
 docker cp ./r10k.yaml puppet:/etc/puppetlabs/r10k/r10k.yaml
 docker cp $sshkeys puppet:/root/
