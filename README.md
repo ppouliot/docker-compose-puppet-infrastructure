74e71d1714eef83ee9ea2c806682991adb351b39
root <root@puppetmaster.bos1.rakops.com>
2018-08-23 11:50:42 -0400

    README.md

--- README.md
+++ README.md
@@ -4,4 +4,23 @@ Docker Compose to run puppet infrastructure with useful modules and a control re
 The goal of this is to stand up a working puppet infrastructure that
 dynamically connects to and deploys a control repo via r10k.
 
+## Basic Operations
 
+
+* List Registered puppet agent certificates
+
+```
+docker exec -t puppet sh -c 'puppet cert list --all'
+```
+
+* Clear a puppet agent certificate
+
+```
+docker exec -t puppet sh -c 'puppet cert clean myserver.contoso.ltd'
+```
+
+* To Run r10k to update puppet modules
+
+```
+docker exec -t puppet sh -c 'cd /etc/puppetlabs/code/environments/production && r10k puppetfile install --verbose DEBUG2'
+```
--- bin/setup.sh
+++ bin/setup.sh
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
+
+base_dir=`pwd`
 echo "Please enter the path to your sh key files"
 read sshkeys
 
-
+git clone git@github.com:ppouliot/secure
+mkdir -p /opt/puppet
+cd /opt/puppet
+git clone https://github.com/rnelson0/puppet-generate-puppetfile
+git clone https://github.com/camptocamp/puppetfile-updater
+git clone https://github.com/dcosson/r10kdiff
+git clone https://github.com/pzim/reaktor
+git clone https://github.com/alister/guard-puppet-lint
+git clone https://github.com/camptocamp/puppet-ghostbuster
+git clone https://github.com/voxpupuli/pypuppetdb
+cd $base_dir
 docker-compose up -d ;
 docker exec -t puppet sh -c 'puppetserver gem install msgpack'
 docker exec -t puppet sh -c 'gem install generate-puppetfile ra10ke hiera-eyaml slack-notifier'
