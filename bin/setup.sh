48aa145ba82eabedf2bfb283b06916df42177f7b
root <root@puppetmaster.bos1.rakops.com>
2018-10-22 20:21:29 +0000

    fixing

--- bin/setup.sh
+++ bin/setup.sh
@@ -7,12 +7,14 @@ read sshkeys
 git clone git@github.com:ppouliot/secure
 cd code/environments/
 docker-compose up -d ;
-sleep 30
+sleep 300
 docker exec -t puppet sh -c 'puppetserver gem install msgpack'
+sleep 60
 docker exec -t puppet sh -c 'gem install generate-puppetfile ra10ke hiera-eyaml slack-notifier'
+sleep 60
 docker exec -t puppet sh -c 'mkdir -p /etc/puppetlabs/r10k'
 docker exec -t puppet sh -c 'mv /etc/puppetlabs/code/environments/production /etc/puppetlabs/code/environments/production.orig'
 docker cp ./hiera.yaml puppet:/etc/puppetlabs/hiera.yaml
-docker cp ./r10k.yaml puppet:/etc/puppetlabs/r10k/r10k.yaml
+docker cp ./r10k/r10k.yaml puppet:/etc/puppetlabs/r10k/r10k.yaml
 docker cp $sshkeys puppet:/root/
 
